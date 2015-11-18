28ac2e2c7955a4f80340d44b68b6388d31e09fbf
Codrin Diaconu <diaconu.codrin@gmail.com>
2015-11-18 21:48:43 +0000

    Added a call back to fortune.js And included fortune.js in the fortune-server.js

--- fortune-server.js
+++ fortune-server.js
@@ -4,7 +4,9 @@ var http = require('http');
 
 //Created the listener
 var requestListener = function(req, res){
-    res.end("The shoe that fits one person pinches another; there is no recipe for living that suits all cases.")
+    fortune(function(quote){
+       res.end(quote); 
+    })
 }
 
 //The actual server
--- library/fortune.js
+++ library/fortune.js
@@ -1,12 +1,14 @@
 module.exports = {
-    getFortune: function(){
+    getFortune: function(cb){
         var request = require('request');
         var url = 'http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en';
         var randomQuote;
         request({url: url}, function(error, response, body){
             var parsed = JSON.parse(body);
             randomQuote = parsed.quoteText;
+            console.log(body);
             console.log(randomQuote);
+            cb(randomQuote);
         })
     }
 }

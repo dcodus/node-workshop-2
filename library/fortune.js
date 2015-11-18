e78c3625abe1c27f39d07c19cb0a63d58c476ea3
Codrin Diaconu <diaconu.codrin@gmail.com>
2015-11-18 20:27:06 +0000

    Imported fortune.js

--- library/fortune.js
+++ library/fortune.js
@@ -1,7 +1,7 @@
 module.exports = {
     getFortune: function(){
         var request = require('request');
-        var url = 'http://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&jsonp=parseQuote&lang=en';
+        var url = 'http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en';
         var randomQuote;
         request({url: url}, function(error, response, body){
             var parsed = JSON.parse(body);

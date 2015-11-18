199d39df8ebd7d3c3a3467cdbe79bc541106a528
Codrin Diaconu <diaconu.codrin@gmail.com>
2015-11-18 21:14:26 +0000

    Added the timer

--- server-delayed.js
+++ server-delayed.js
@@ -1,9 +1,10 @@
 var http = require('http');
 
 var requestListener = function(req, res){
+    console.log(req.url);
     setTimeout(function(){
         res.end("<h1>HELLO THERE GOOD PEOPLE</h1>");
-    })
+    },10000)
 }
 
 var server = http.createServer(requestListener);

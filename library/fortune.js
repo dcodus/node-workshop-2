64c9a34aaccc3d9f4ef7dfabd0dcba5d7c338d0c
Codrin Diaconu <diaconu.codrin@gmail.com>
2015-11-18 20:09:08 +0000

    Added the fortune.js fortune function

---
+++ library/fortune.js
@@ -0,0 +1,14 @@
+module.exports = {
+    getFortune: function(){
+        var request = require('request');
+        var url = 'http://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&jsonp=parseQuote&lang=en';
+        var randomQuote;
+        request({url: url}, function(error, response, body){
+            var parsed = JSON.parse(body);
+            randomQuote = parsed.quoteText;
+            console.log(randomQuote);
+        })
+    }
+}
+
+

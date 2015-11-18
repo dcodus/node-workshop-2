65f98330d54e0bd287b250417a8c24b3552750f1
Codrin Diaconu <diaconu.codrin@gmail.com>
2015-11-18 23:03:39 +0000

    Added the cached-location function

---
+++ cached-location-server.js
@@ -0,0 +1,32 @@
+var apiKey = "AIzaSyBzCi__81f6T97pDZJDwaatlZOnPpZjo_o";
+
+var request = require("request");
+var http = require('http');
+var cached = {};
+
+//Created the listener
+var requestListener = function(req, res){
+    //Passing to res the string "hello world"
+    if(req.url.substr(1) !== 'favicon.ico'){
+        var location = req.url.substr(1).toLowerCase();
+            if(!cached[location]){
+                var urlLoc = "https://maps.googleapis.com/maps/api/geocode/json?address="+ encodeURIComponent(location)+"?key=AIzaSyBzCi__81f6T97pDZJDwaatlZOnPpZjo_o";
+                request({url: urlLoc}, function(err, response, body){
+                    var parsedBody = JSON.parse(body);
+                    var lat = parsedBody.results[0].geometry.location.lat;
+                    var lng = parsedBody.results[0].geometry.location.lng;
+                    var locationOutput = "Based on your input, you are located at a latitude of " + lat.toString()+ " and a longitude of " +lng.toString()+ ".";
+                    //When we want to create a object but dont know the name we use the [] instead of the .
+                    cached[location] = {lat: lat, lng: lng}
+                    res.end(locationOutput);
+                })
+            } else {
+                var cachedCity = "Based on your input, you are located at a latitude of " +cached[location].lat+ " and a longitude of " +cached[location].lng+ ". This result is a cached one.";
+                res.end(cachedCity);
+            }
+    }
+}
+
+//The actual server
+var server = http.createServer(requestListener);
+server.listen(process.env.PORT);
